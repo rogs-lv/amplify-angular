b6afbe78433be16b52b774b434425e115f973e64
rogs <oliverrosasg@gmail.com>
2023-12-13 12:44:23 -0600

    Complete configurations

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "appamplifyangular48ff5d07": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Amplify } from 'aws-amplify';
-import { amplifyconfig } from '../amplifyconfiguration.json';
-
-Amplify.configure(amplifyconfig);
 
 @NgModule({
   declarations: [
